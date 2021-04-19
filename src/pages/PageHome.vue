6e164cadfcc291d29c0e231a79b7ddeb41e70611
Rokas <rookas.rudzenskas@gmail.com>
2021-04-19 14:07:24 +0300

    Rokas

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,40 +1,65 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="card-post q-mb-md"
-      flat
-      bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="../assets/rokas.png">
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="../assets/rokas.png">
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label
-          class="text-bold"
-          >
-            rookasrudzianskas
-          </q-item-label>
-          <q-item-label caption>
-            {{ post.location }}
-          </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label
+                class="text-bold"
+              >
+                rookasrudzianskas
+              </q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
+          <q-separator />
 
-      <q-img :src="post.imgUrl"/>
+          <q-img :src="post.imgUrl"/>
 
-      <q-card-section>
-        <div>{{ post.caption}}</div>
-        <div class="text-caption text-grey">{{ post.date | niceDate}}</div>
-      </q-card-section>
+          <q-card-section>
+            <div>{{ post.caption}}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate}}</div>
+          </q-card-section>
 
-    </q-card>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="../assets/rokas.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label
+              class="text-bold"
+            >
+              rookasrudzianskas
+            </q-item-label>
+            <q-item-label caption>
+              Rookas Rudzianskas
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+
   </q-page>
 </template>
 
