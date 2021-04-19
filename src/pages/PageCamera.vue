546ec0aaaf10b94046117847c71883fba3cfa3fa
Rokas <rookas.rudzenskas@gmail.com>
2021-04-19 14:24:08 +0300

    Rokas

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,6 +1,45 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img
+        src="../assets/rokas.png"
+        class="full-width"
+        alt="">
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        round
+        color="grey-10"
+        icon="eva-camera-outline"
+        size="lg"
+        />
+
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="text"
+          label="Caption"
+          class="col col-sm-6"
+          dense/>
+      </div>
+
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="text"
+          label="Location"
+          class="col col-sm-6"
+          dense>
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+
+        </q-input>
+      </div>
+
+      <div class="row justify-center q-mt-lg">
+        <q-btn unelevated rounded color="primary" label="Post Image" />
+      </div>
+
+    </div>
   </q-page>
 </template>
 
@@ -9,3 +48,9 @@ export default {
   name: 'PageCamera'
 }
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
