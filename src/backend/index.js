3fc168c81dfc42affbdcff70faf38728f89ead2b
Rokas <rookas.rudzenskas@gmail.com>
2021-04-20 13:39:11 +0300

    Rokas

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,7 +3,8 @@
 
 const express = require('express')
 const admin = require('firebase-admin');
-
+let inspect = require('util').inspect;
+let Busboy = require('busboy');
 
 
 //config - express
@@ -41,7 +42,30 @@ const db = admin.firestore();
 
 app.post('/createPost', (request, response) => {
   response.set('Access-Control-Allow-Origin', '*')
-  response.send(request.headers)
+
+
+  var busboy = new Busboy({ headers: request.headers });
+
+  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+    file.on('data', function(data) {
+      console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+    });
+    file.on('end', function() {
+      console.log('File [' + fieldname + '] Finished');
+    });
+  });
+
+  busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+    console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+  });
+
+  busboy.on('finish', function() {
+    console.log('Done parsing form!');
+    // response.writeHead(303, { Connection: 'close', Location: '/' });
+    response.send("Done parcing form!")
+  });
+  request.pipe(busboy);
 })
 
 
